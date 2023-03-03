61017db5b1c82ac18ceb90f3918d4d8222a518c4
denis <106389581+fishman123456@users.noreply.github.com>
2023-03-03 19:24:18 +0700

    Initial commit

--- Program.cs
+++ Program.cs
@@ -2,6 +2,18 @@
 
 using _03_03_2023_linq_forum;
 
-human d = new human("петр", "петров", 25);
+human d = new human("эдуард", "коляка", 46);
 
 Console.WriteLine(d);
+human[] peoples = {
+    new human("петр", "петров", 36),
+    new human("иван", "федоров", 45),
+    new human("олег", "киселев", 52),
+    new human("андрей", "туралин", 41),
+    new human("сергей", "сырцов", 62),
+    new human("валерий", "мельников", 56),
+    new human("евгений", "тарасов", 43),
+    new human("Иван", "шалаев", 35),
+    new human("ольга", "еременко", 43),
+    new human("света", "пивченко", 56)
+    };
