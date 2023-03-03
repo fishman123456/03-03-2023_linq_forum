bce8803afd654899e6b62382e4dc97b1300e4e33
denis <106389581+fishman123456@users.noreply.github.com>
2023-03-03 19:23:24 +0700

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,7 @@
+﻿
+
+using _03_03_2023_linq_forum;
+
+human d = new human("петр", "петров", 25);
+
+Console.WriteLine(d);
---
+++ human.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _03_03_2023_linq_forum
+{
+    public class human
+    {
+        string name{ get; set; }
+        string soneme { get; set; }
+        int age { get; set; }
+        public human(string _name, string _soneme, int _age)
+        {
+           name = _name;
+          soneme = _soneme;
+           age = _age;
+        }
+        public human() { }
+
+        public override string ToString()
+        {
+            return name +" " + soneme+ " "+ age;
+        }
+    }
+}
